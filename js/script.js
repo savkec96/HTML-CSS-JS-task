159142ad0765c966d65afb9a245614b7728f2430
savkec96 <savoj96@gmail.com>
2022-07-04 16:05:37 +0200

    Changes with modal!

--- js/script.js
+++ js/script.js
@@ -105,7 +105,6 @@ btnRight2.addEventListener('click', function () {
 });
 
 // Swiper animation
-
 let slider = document.querySelector('.sliders');
 let innerSlider = document.querySelector('.slide-track');
 
@@ -154,83 +153,42 @@ function checkboundary() {
 }
 
 // Modal animation
-// const modal = document.querySelectorAll('.modal');
-// const btnCloseModal = document.querySelector('.close-modal');
-// const overlay = document.querySelector('.overlay');
-// const btnsOpenModal = document.querySelectorAll('.show-modal');
+const matthew = document.querySelector('.matthew');
+const cristopher = document.querySelector('.cristopher');
+const michael = document.querySelector('.michael');
+const btnCloseModal = document.querySelectorAll('.close-modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-// const openingModal = function (e) {
-//   e.preventDefault();
-//   modal.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// };
+const openingModal = function (mod) {
+  if (mod.getAttribute('data-name') === 'matthew') {
+    matthew.classList.remove('hidden');
+  } else if (mod.getAttribute('data-name') === 'cristopher') {
+    cristopher.classList.remove('hidden');
+  } else if (mod.getAttribute('data-name') === 'michael') {
+    michael.classList.remove('hidden');
+  }
+  overlay.classList.remove('hidden');
+};
 
-// const openingModal = function (e) {
-//   e.preventDefault();
-//   for (let i = 0; i < modal.length; i++) {
-//     if (modal[i].getAttribute('data-name') == 'matthew') {
-//       modal[i].classList.remove('hidden');
-//     } else if (modal[i].getAttribute('data-name') == 'cristopher') {
-//       modal[i].classList.remove('hidden');
-//     } else if (modal[i].getAttribute('data-name') == 'michael') {
-//       modal[i].classList.remove('hidden');
-//     }
-//     overlay.classList.remove('hidden');
-//   }
-// };
+const closeModal = function (mod) {
+  const people = [matthew, cristopher, michael];
+  for (mod = 0; mod < people.length; mod++) {
+    if (!people[mod].classList.contains('hidden')) {
+      people[mod].classList.add('hidden');
+    }
+  }
+  overlay.classList.add('hidden');
+};
 
-// const closeModal = function () {
-//   modal.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
+btnsOpenModal.forEach(item => {
+  item.addEventListener('click', function () {
+    openingModal(item);
+  });
+});
 
-// btnsOpenModal.addEventListener('click', openingModal);
-
-// btnCloseModal.addEventListener('click', closeModal);
-
-/////////////////////////////////// OVAKO RADI ALI NIJE CISTO!!!! ///////////////////////////////////////////
-
-// const modal1 = document.querySelector('.modal1');
-// const modal2 = document.querySelector('.modal2');
-// const modal3 = document.querySelector('.modal3');
-// const overlay = document.querySelector('.overlay');
-// const btnCloseModal1 = document.querySelector('.close-modal1');
-// const btnCloseModal2 = document.querySelector('.close-modal2');
-// const btnCloseModal3 = document.querySelector('.close-modal3');
-// const openModal1 = document.querySelector('.show-modal1');
-// const openModal2 = document.querySelector('.show-modal2');
-// const openModal3 = document.querySelector('.show-modal3');
-
-// const openingModal1 = function () {
-//   modal1.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// };
-// const openingModal2 = function () {
-//   modal2.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// };
-// const openingModal3 = function () {
-//   modal3.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// };
-
-// const closingModal1 = function () {
-//   modal1.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
-// const closingModal2 = function () {
-//   modal2.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
-// const closingModal3 = function () {
-//   modal3.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
-
-// openModal1.addEventListener('click', openingModal1);
-// openModal2.addEventListener('click', openingModal2);
-// openModal3.addEventListener('click', openingModal3);
-
-// btnCloseModal1.addEventListener('click', closingModal1);
-// btnCloseModal2.addEventListener('click', closingModal2);
-// btnCloseModal3.addEventListener('click', closingModal3);
+btnCloseModal.forEach(item => {
+  item.addEventListener('click', function () {
+    closeModal(item);
+  });
+});
